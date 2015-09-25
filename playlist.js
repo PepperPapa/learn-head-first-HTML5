7253131b55fab9edca8074a7791cea82b8370ed6
unknown <w17390@W17390.wasion.cn>
2015-09-25 08:16:48 +0800

    update playlist with event handle

---
+++ playlist.js
@@ -0,0 +1,10 @@
+window.onload = init;
+function init() {
+    var button = document.getElementById("addButton");
+    button.onclick =  handleButtonClick;
+}
+function handleButtonClick() {
+    var textInput = document.getElementById("songTextInput");
+    var songName = textInput.value;
+    alert("Adding " + songName);
+}
