82189bcb06c5478c9d3be1feb2c11cb590e56255
pepperpapa <zhongxin2506@outlook.com>
2015-09-29 18:56:56 +0800

    finished playlist app

--- playlist.js
+++ playlist.js
@@ -2,9 +2,14 @@ window.onload = init;
 function init() {
     var button = document.getElementById("addButton");
     button.onclick =  handleButtonClick;
+    loadPlaylist();
 }
 function handleButtonClick() {
     var textInput = document.getElementById("songTextInput");
     var songName = textInput.value;
-    alert("Adding " + songName);
+    var li = document.createElement("li");
+    li.innerHTML = songName;
+    var ul = document.getElementById("playlist");
+    ul.appendChild(li);
+    save(songName);
 }
